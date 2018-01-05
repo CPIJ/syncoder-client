26d2d6397169289d9eacfa22cf30d6b0d72539ec
Casper Pijnenburg <casperpijnenburg@gmail.com>
2018-01-05 14:47:45 +0100

    Added stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { LoginComponent } from './component/login/login.component';
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './component/register/register.component';
 import { HomeComponent } from './component/home/home.component';
+import { EditorComponent } from './component/editor/editor.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'register', component: RegisterComponent},
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'edit', component: EditorComponent }
 ];
