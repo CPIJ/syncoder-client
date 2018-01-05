32cc4d3fb199ba02cfa625076b4a0604d194087c
Casper Pijnenburg <casperpijnenburg@gmail.com>
2018-01-05 12:55:22 +0100

    Adds route for home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
-import { LoginComponent } from "./component/login/login.component";
-import { Routes } from '@angular/router'
-import { RegisterComponent } from "./component/register/register.component";
+import { LoginComponent } from './component/login/login.component';
+import { Routes } from '@angular/router';
+import { RegisterComponent } from './component/register/register.component';
+import { HomeComponent } from './component/home/home.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'register', component: RegisterComponent},
-]
+    { path: 'home', component: HomeComponent }
+];
